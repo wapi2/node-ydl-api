fc60734df33599e5e2b82ba0b3732969920d0845
Jhon Grandez <admin@wapi2.com>
2025-01-20 12:30:50 -0500

    what:server created!

---
+++ auth.middleware.js
@@ -0,0 +1,18 @@
+const config = require('./config');
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Token no proporcionado' });
+    }
+
+    if (token !== config.API_TOKEN) {
+        return res.status(403).json({ error: 'Token inválido' });
+    }
+
+    next();
+}
+
+module.exports = authenticateToken;
---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+const cors = require("cors");
+const authenticateToken = require('./auth.middleware');
+
+const app = express();
+app.use(cors());
+app.use(['/info', '/mp3', '/mp4'], authenticateToken);
+
+app.get("/", (req, res) => {
+    const ping = new Date();
+    ping.setHours(ping.getHours() - 3);
+    console.log(
+        `Ping at: ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
+    );
+    res.sendStatus(200);
+});
+
+app.get("/info", async (req, res) => {
+    const { url } = req.query;
+
+    if (url) {
+        const isValid = ytdl.validateURL(url);
+
+        if (isValid) {
+            const info = (await ytdl.getInfo(url)).videoDetails;
+
+            const title = info.title;
+            const thumbnail = info.thumbnails[2].url;
+
+            res.send({ title: title, thumbnail: thumbnail });
+        } else {
+            res.status(400).send("Invalid url");
+        }
+    } else {
+        res.status(400).send("Invalid query");
+    }
+});
+
+app.get("/mp3", async (req, res) => {
+    const { url } = req.query;
+
+    if (url) {
+        const isValid = ytdl.validateURL(url);
+
+        if (isValid) {
+            const videoName = (await ytdl.getInfo(url)).videoDetails.title;
+
+            res.header(
+                "Content-Disposition",
+                `attachment; filename="${videoName}.mp3"`
+            );
+            res.header("Content-type", "audio/mpeg3");
+
+            ytdl(url, { quality: "highestaudio", format: "mp3" }).pipe(res);
+        } else {
+            res.status(400).send("Invalid url");
+        }
+    } else {
+        res.status(400).send("Invalid query");
+    }
+});
+
+app.get("/mp4", async (req, res) => {
+    const { url } = req.query;
+
+    if (url) {
+        const isValid = ytdl.validateURL(url);
+
+        if (isValid) {
+            const videoName = (await ytdl.getInfo(url)).videoDetails.title;
+
+            res.header(
+                "Content-Disposition",
+                `attachment; filename="${videoName}.mp4"`
+            );
+
+            ytdl(url, {
+                quality: "highest",
+                format: "mp4",
+            }).pipe(res);
+        } else {
+            res.status(400).send("Invalid url");
+        }
+    } else {
+        res.status(400).send("Invalid query");
+    }
+});
+
+app.listen(process.env.PORT || 3500, () => {
+    console.log("Server on");
+});
