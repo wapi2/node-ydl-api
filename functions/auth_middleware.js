e0e7b8bfd42f498a8eadeddb97095d167664e0dd
Jhon Grandez <admin@wapi2.com>
2025-01-20 19:27:19 -0500

    Fix dependencies

--- functions/auth_middleware.js
+++ functions/auth_middleware.js
@@ -1,23 +1,19 @@
+// auth_middleware.js
 import config from './config.js';
 
 const authenticateToken = (req, res, next) => {
-    try {
-        const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(' ')[1];
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
-        if (!token) {
-            return res.status(401).json({ error: 'Token no proporcionado' });
-        }
-
-        if (token !== config.API_TOKEN) {
-            return res.status(403).json({ error: 'Token inválido' });
-        }
-
-        next();
-    } catch (error) {
-        console.error('Error en autenticación:', error);
-        res.status(500).json({ error: 'Error interno del servidor' });
+    if (!token) {
+        return res.status(401).json({ error: 'Token no proporcionado' });
     }
+
+    if (token !== config.API_TOKEN) {
+        return res.status(403).json({ error: 'Token inválido' });
+    }
+
+    next();
 };
 
-export default authenticateToken;
+export { authenticateToken };
--- functions/config.js
+++ functions/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_TOKEN: process.env.API_TOKEN || "OSDZ07/5WEx1h-4ZEc?zcdfdIBqjAhx20200O9VwATpypBcGw1vb!?=d4OIrQLYy/96!q5ozRAxu=muvjKGbtClIkm6YpKxPDm21L8GZjGs6Cuek883SzMj6Gbf3!wZ1Bw8SzBuIalEFAKajfIPIjU2fIc5re73rag-4V7lnGHQ7VKpn0JtCUptEsdo?ng/16=kWN47PwBcnZtPl6p5uDNI/BpJOGlx5g-soX?DnZa?Yi!lrcuEXOf/y4Gk=yf7H"
+    API_TOKEN: "OSDZ07/5WEx1h-4ZEc?zcdfdIBqjAhx20200O9VwATpypBcGw1vb!?=d4OIrQLYy/96!q5ozRAxu=muvjKGbtClIkm6YpKxPDm21L8GZjGs6Cuek883SzMj6Gbf3!wZ1Bw8SzBuIalEFAKajfIPIjU2fIc5re73rag-4V7lnGHQ7VKpn0JtCUptEsdo?ng/16=kWN47PwBcnZtPl6p5uDNI/BpJOGlx5g-soX?DnZa?Yi!lrcuEXOf/y4Gk=yf7H"
 };
 
 export default config;
